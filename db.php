9fa8f34e256f7233e7a2b0629f344a0ea2684811
Tarvi Tihhanov <miilits@gmail.com>
2017-06-04 11:24:41 +0300

    syntax errorid

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
  // Connects to your Database 
  mysqli_connect("localhost", "test", "t3st3r123") or die(mysqli_error()); 
- mysqli_select_db("test") or die(mysqli_error()); 
+ mysqli_select_db("test"); 
 //Lisab yhe kylastuse counterile juurde
 
 mysqli_query("UPDATE counter_ttihhano SET counter = counter + 1");
