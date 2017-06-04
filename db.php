114a3c42fa2ebe98716032c17da1dbcc644df84a
Tarvi Tihhanov <miilits@gmail.com>
2017-06-04 11:17:26 +0300

    syntax errorid

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
  // Connects to your Database 
- mysql_connect("localhost", "test", "t3st3r123") or die(mysql_error()); 
- mysql_select_db("test") or die(mysql_error()); 
+ mysqli_connect("localhost", "test", "t3st3r123") or die(mysql_error()); 
+ mysqli_select_db("test") or die(mysql_error()); 
 //Lisab yhe kylastuse counterile juurde
 
-mysql_query("UPDATE counter_ttihhano SET counter = counter + 1");
+mysqli_query("UPDATE counter_ttihhano SET counter = counter + 1");
 
 //V6tab numbri baasist
 
-$count = mysql_fetch_row(mysql_query("SELECT counter FROM counter"));
+$count = mysqli_fetch_row(mysql_query("SELECT counter FROM counter"));
 
 //N2itan palju kylastatud
 
