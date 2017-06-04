d2616026d6be5220f6e52cd759d3ef22ee0002f3
Tarvi Tihhanov <miilits@gmail.com>
2017-06-04 11:24:19 +0300

    syntax errorid

--- db.php
+++ db.php
@@ -1,23 +1,17 @@
 <?php
+ // Connects to your Database 
+ mysqli_connect("localhost", "test", "t3st3r123") or die(mysqli_error()); 
+ mysqli_select_db("test") or die(mysqli_error()); 
+//Lisab yhe kylastuse counterile juurde
 
-    $host = "localhost";
-    $user = "test";
-    $pass = "t3st3r123";
-    $db = "test";
-    $l = mysqli_connect($host, $user, $pass, $db);
-    mysqli_query($l, "SET CHARACTER SET UTF8") or
-            die("Error, ei saa andmebaasi charsetti seatud");
-            $sql = "UPDATE counter_ttihhano SET counter = counter + 1";
-            $result = $l->query($sql);
-    //V6tab numbri baasist
+mysqli_query("UPDATE counter_ttihhano SET counter = counter + 1");
+
+//V6tab numbri baasist
 
 $count = mysqli_fetch_row(mysqli_query("SELECT counter FROM counter"));
 
 //N2itan palju kylastatud
 
 print "$count[0]";
-
-    mysqli_close($l);
-
 
 ?>
